3a2cf69c326ed2d48fb9857977256079738bcca8
KseniaLa <ksla1998@mail.ru>
2019-11-17 15:01:51 +0300

    change to shift memory

--- stopwatch/memory.vhd
+++ stopwatch/memory.vhd
@@ -40,7 +40,9 @@ entity memory is
 		WriteEn	: in  STD_LOGIC;
 		DataIn	: in  STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
 		ReadEn	: in  STD_LOGIC;
-		DataOut	: out STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0)
+		DataOut	: out STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
+		ReadEnd	: out STD_LOGIC;
+		Empty	: out STD_LOGIC
 	);
 end memory;
 
@@ -51,50 +53,34 @@ begin
 		type FIFO_Memory is array (0 to FIFO_DEPTH - 1) of STD_LOGIC_VECTOR (DATA_WIDTH - 1 downto 0);
 		variable Memory : FIFO_Memory;
 		
-		variable ReadPtr : natural range 0 to FIFO_DEPTH - 1;
+		variable ReadPtr : integer range -1 to FIFO_DEPTH - 1;
 		variable WritePtr : natural range 0 to FIFO_DEPTH - 1;
-		variable Full : boolean;
 	begin
 		if rising_edge(CLK) then
 			if RST = '1' then
-				ReadPtr := 0;
+				ReadPtr := FIFO_DEPTH - 1;
 				WritePtr := 0;
-				Full := false;
+				
+				ReadEnd <= '0';
 			else
 				if (ReadEn = '1') then
-					if (ReadPtr /= WritePtr) then
-						DataOut <= Memory(ReadPtr);
-						
-						if (ReadPtr = FIFO_DEPTH - 1) then
-							ReadPtr := 0;
-						else
-							ReadPtr := ReadPtr + 1;
-						end if;
+					DataOut <= Memory(ReadPtr);
+					
+					if (ReadPtr = 0) then
+						ReadPtr := FIFO_DEPTH - 1;
+						ReadEnd <= '1';
+					else
+						ReadPtr := ReadPtr - 1;
 					end if;
 				end if;
 				
 				if (WriteEn = '1') then
-						if (Full) then
-							WritePtr := FIFO_DEPTH - 1;
-							
-							-- or ReadPtr := 0 ???
-							if (ReadPtr > 0) then
-								ReadPtr := ReadPtr - 1;
-							end if;
-							
-							for ptr in 1 to FIFO_DEPTH - 1 loop
-								Memory(ptr - 1) := Memory(ptr);
-							end loop;
-						end if;
+						for ptr in FIFO_DEPTH - 2 downto 0 loop
+							Memory(ptr + 1) := Memory(ptr);
+						end loop;
 
 						Memory(WritePtr) := DataIn;
-						
-						if (WritePtr = FIFO_DEPTH - 1) then
-							Full := true;
-						else
-							WritePtr := WritePtr + 1;
-						end if;
-
+						ReadPtr := FIFO_DEPTH - 1;
 				end if;
 				
 			end if;
