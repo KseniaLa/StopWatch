7e7a3ee8a5bb242382794d834a4109588aa4f860
KseniaLa <ksla1998@mail.ru>
2019-11-30 18:57:55 +0300

    update digit drawing

--- stopwatch/vga_controller.vhd
+++ stopwatch/vga_controller.vhd
@@ -32,61 +32,57 @@ use ieee.std_logic_unsigned.all;
 
 ENTITY vga_controller IS
 	GENERIC(
-		h_pulse 	:	INTEGER := 120;    	--horiztonal sync pulse width in pixels
-		h_bp	 	:	INTEGER := 64;		--horiztonal back porch width in pixels
-		h_pixels	:	INTEGER := 800;		--horiztonal display width in pixels
-		h_fp	 	:	INTEGER := 56;		--horiztonal front porch width in pixels
-		h_pol		:	STD_LOGIC := '0';		--horizontal sync pulse polarity (1 = positive, 0 = negative)
-		v_pulse 	:	INTEGER := 6;			--vertical sync pulse width in rows
-		v_bp	 	:	INTEGER := 23;			--vertical back porch width in rows
-		v_pixels	:	INTEGER := 600;		--vertical display width in rows
-		v_fp	 	:	INTEGER := 37;			--vertical front porch width in rows
-		v_pol		:	STD_LOGIC := '1');	--vertical sync pulse polarity (1 = positive, 0 = negative)
+		h_pulse 	:	INTEGER := 120;
+		h_bp	 	:	INTEGER := 64;
+		h_pixels	:	INTEGER := 800;
+		h_fp	 	:	INTEGER := 56;	
+		h_pol		:	STD_LOGIC := '0';
+		v_pulse 	:	INTEGER := 6;
+		v_bp	 	:	INTEGER := 23;
+		v_pixels	:	INTEGER := 600;
+		v_fp	 	:	INTEGER := 37;
+		v_pol		:	STD_LOGIC := '1');
 	PORT(
-		pixel_clk	:	IN		STD_LOGIC;	--pixel clock at frequency of VGA mode being used
-		h_sync		:	OUT	STD_LOGIC;	--horiztonal sync pulse
-		v_sync		:	OUT	STD_LOGIC;	--vertical sync pulse
+		pixel_clk	:	IN		STD_LOGIC;
+		h_sync		:	OUT	STD_LOGIC;
+		v_sync		:	OUT	STD_LOGIC;
 		red : out  STD_LOGIC_VECTOR (4 downto 0);
       green : out  STD_LOGIC_VECTOR (5 downto 0);
       blue : out  STD_LOGIC_VECTOR (4 downto 0));
 END vga_controller;
 
 ARCHITECTURE behavior OF vga_controller IS
-	CONSTANT	h_period	:	INTEGER := h_pulse + h_bp + h_pixels + h_fp;  --total number of pixel clocks in a row
-	CONSTANT	v_period	:	INTEGER := v_pulse + v_bp + v_pixels + v_fp;  --total number of rows in column
+	CONSTANT	h_period	:	INTEGER := h_pulse + h_bp + h_pixels + h_fp;
+	CONSTANT	v_period	:	INTEGER := v_pulse + v_bp + v_pixels + v_fp;
 BEGIN
 
 	PROCESS(pixel_clk)
-		VARIABLE h_count	:	INTEGER RANGE 0 TO h_period - 1 := 0;  --horizontal counter (counts the columns)
-		VARIABLE v_count	:	INTEGER RANGE 0 TO v_period - 1 := 0;  --vertical counter (counts the rows)
+		VARIABLE h_count	:	INTEGER RANGE 0 TO h_period - 1 := 0;
+		VARIABLE v_count	:	INTEGER RANGE 0 TO v_period - 1 := 0;
 	BEGIN
 	
 		IF(pixel_clk'EVENT AND pixel_clk = '1') THEN
-
-			--counters
-			IF(h_count < h_period - 1) THEN		--horizontal counter (pixels)
+			IF(h_count < h_period - 1) THEN
 				h_count := h_count + 1;
 			ELSE
 				h_count := 0;
-				IF(v_count < v_period - 1) THEN	--veritcal counter (rows)
+				IF(v_count < v_period - 1) THEN	
 					v_count := v_count + 1;
 				ELSE
 					v_count := 0;
 				END IF;
 			END IF;
 
-			--horizontal sync signal
 			IF(h_count < h_pixels + h_fp OR h_count >= h_pixels + h_fp + h_pulse) THEN
-				h_sync <= NOT h_pol;		--deassert horiztonal sync pulse
+				h_sync <= '1';--NOT h_pol;		
 			ELSE
-				h_sync <= h_pol;			--assert horiztonal sync pulse
+				h_sync <= '0';--h_pol;			
 			END IF;
 			
-			--vertical sync signal
 			IF(v_count < v_pixels + v_fp OR v_count >= v_pixels + v_fp + v_pulse) THEN
-				v_sync <= NOT v_pol;		--deassert vertical sync pulse
+				v_sync <= '1';--NOT v_pol;		
 			ELSE
-				v_sync <= v_pol;			--assert vertical sync pulse
+				v_sync <= '0';--v_pol;			
 			END IF;
 			
 			if (h_count > h_pixels + h_fp) then
@@ -94,17 +90,23 @@ BEGIN
 				green <= "000000";
 				blue <= "00000";
 			else
-				if (((h_count > 20) and (h_count < 60)) and ((v_count > 20) and (v_count < 50))) then
-					red <= "11111";
-					green <= "000000";
-					blue <= "00000";
+				if (((h_count > 19) and (h_count < 28)) and ((v_count > 19) and (v_count < 36))) then
+					if (((h_count >=22)  and (h_count <= 25) and (v_count >=22)  and (v_count <= 26)) or 
+					   ((h_count >=22)  and (h_count <= 25) and (v_count >=29)  and (v_count <= 33))) then
+						red <= "00000";
+						green <= "011111";
+						blue <= "00000";
+					else
+						red <= "11111";
+						green <= "000000";
+						blue <= "00000";
+					end if;
 				else
 					red <= "00000";
 					green <= "011111";
 					blue <= "00000";
 				end if;
 			end if;
-
 		END IF;
 	END PROCESS;
 
