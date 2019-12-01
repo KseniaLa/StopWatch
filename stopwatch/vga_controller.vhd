b8083d85f04afe2cd5417675294b91598ead0731
KseniaLa <ksla1998@mail.ru>
2019-12-01 18:47:16 +0300

    add digits drawing template

--- stopwatch/vga_controller.vhd
+++ stopwatch/vga_controller.vhd
@@ -21,6 +21,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
+package vga_controller_type is
+        type data_array is array(0 to 71) of integer;
+end package;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+use work.vga_controller_type.all;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -44,6 +53,7 @@ ENTITY vga_controller IS
 		v_pol		:	STD_LOGIC := '1');
 	PORT(
 		pixel_clk	:	IN		STD_LOGIC;
+		data        :  IN    data_array;
 		h_sync		:	OUT	STD_LOGIC;
 		v_sync		:	OUT	STD_LOGIC;
 		red : out  STD_LOGIC_VECTOR (4 downto 0);
@@ -59,6 +69,24 @@ BEGIN
 	PROCESS(pixel_clk)
 		VARIABLE h_count	:	INTEGER RANGE 0 TO h_period - 1 := 0;
 		VARIABLE v_count	:	INTEGER RANGE 0 TO v_period - 1 := 0;
+		variable h_min : integer := 0; --for square
+		variable h_max : integer := 0; --for square
+		variable v_min : integer := 0; --for square
+		variable v_max : integer := 0; --for square
+		variable col : integer := 0; --for array pos
+		variable row : integer := 0; --for array pos
+		variable data_pos : integer := 0;
+		variable curr_digit: integer := 0;
+		
+		variable h_sq11 : integer := 0;
+		variable h_sq12 : integer := 0;
+		variable h_sq21 : integer := 0;
+		variable h_sq22 : integer := 0;
+		
+		variable v_sq11 : integer := 0;
+		variable v_sq12 : integer := 0;
+		variable v_sq21 : integer := 0;
+		variable v_sq22 : integer := 0;
 	BEGIN
 	
 		IF(pixel_clk'EVENT AND pixel_clk = '1') THEN
@@ -85,14 +113,61 @@ BEGIN
 				v_sync <= '0';--v_pol;			
 			END IF;
 			
+			if ((h_count > 2) and (h_count < 3)) then -- 1
+				col := 0;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 2
+				col := 1;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 3
+				col := 2;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 4
+				col := 3;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 5
+				col := 4;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 6
+				col := 5;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 7
+				col := 6;
+			elsif ((h_count > 2) and (h_count < 3)) then -- 8
+				col := 7;
+			else
+				col := 0;
+			end if;
+			
+			if ((v_count > 2) and (v_count < 3)) then
+				row := 0;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 1;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 2;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 3;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 4;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 5;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 6;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 7;
+			elsif ((v_count > 2) and (v_count < 3)) then
+				row := 8;
+			else
+				row := 0;
+			end if;
+			
+			data_pos := 8 * row + col; --??????????????????????
+			curr_digit := data(data_pos);
+			
+			-- here!
+			
 			if (h_count > h_pixels + h_fp) then
 				red <= "00000";
 				green <= "000000";
 				blue <= "00000";
 			else
-				if (((h_count > 19) and (h_count < 28)) and ((v_count > 19) and (v_count < 36))) then
-					if (((h_count >=22)  and (h_count <= 25) and (v_count >=22)  and (v_count <= 26)) or 
-					   ((h_count >=22)  and (h_count <= 25) and (v_count >=29)  and (v_count <= 33))) then
+				if (((h_count > h_min) and (h_count < h_max)) and ((v_count > v_min) and (v_count < v_max))) then
+					if (((h_count >= h_sq11)  and (h_count <= h_sq12) and (v_count >= v_sq11)  and (v_count <= v_sq12)) or 
+					   ((h_count >= h_sq21)  and (h_count <= h_sq22) and (v_count >= v_sq21)  and (v_count <= v_sq22))) then
 						red <= "00000";
 						green <= "011111";
 						blue <= "00000";
